b86432f0b59e81f5061293cd0e452b313e3376de
Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
2015-05-03 12:54:36 -0300

    Added support for long options
    Signed-off-by: Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>

--- commander.lua
+++ commander.lua
@@ -29,19 +29,36 @@ function Program:version(version)
   return self
 end
 
-function Program:option(option, description)
- table.insert(self._options, { short = option, description = description } )
+function Program:_splitFlags(flags)
 
- return self
+  return string.match(flags, '(%-%w+)%s*,%s*(%-%-%w+)')
+end
+
+function Program:_snakecase(str)
+  return string.gsub(str, '%-', '_') 
+end
+
+function Program:option(flags, description, fn, default)
+  local short, long = self:_splitFlags(flags)
+  table.insert(self._options, { short = short, long = long, description = description } )
+  return self
 end
 
 function Program:_findShortOption(option)
+  return self:_findOption(option, 'short')
+end
+
+function Program:_findLongOption(option)
+  return self:_findOption(option, 'long')
+end
+
+function Program:_findOption(option, type)
   for _, o in ipairs(self._options) do
-      if option == o.short then
-        return true
-      end
-  end 
-  return false
+    if option == o[type] then
+      return o 
+    end
+  end
+  return nil
 end
 
 function Program:_isShortFlag(flag)
@@ -49,12 +66,29 @@ function Program:_isShortFlag(flag)
   return string.match(flag, '^(%-[^%-]+)$')
 end
 
+function Program:_isLongFlag(flag)
+  assert(flag, '_isLongFlag exepct non-nil value')
+  return string.match(flag, '^(%-%-[^%-]+)$')
+end
+
+function Program:_extractShortFlag(flag)
+  return string.match(flag, '^-(%w+)$')
+end
+
+function Program:_extractLongFlag(flag)
+  return string.match(flag, '^%-%-(%w+)$')
+end
+
+function Program:charAt(str, i)
+  return string.sub(str, i, i)
+end
+
 function Program:_expandShortFlag(flag)
   assert(flag, '_expandShortFlag expect non-nil flag')
   local t = {}
   local i = 2 
   while true do
-    local f = string.sub(flag, i, i) 
+    local f = self:charAt(flag, i) 
     if f == '' then break end
     
     table.insert(t, '-' .. f)
@@ -67,29 +101,40 @@ function Program:_addIgnoredArg(arg)
   table.insert(self.args, arg)
 end
 
-function Program:_setField(flag)
-  local field = string.match(flag, '%-([a-zA-Z])')
-    if field then
-      self[field] = true
-    end
+function Program:_setField(option)
+  local shortField = self:_extractShortFlag(option.short)
+  local longField = self:_extractLongFlag(option.long)
+  self[shortField] = true
+  self[longField] = true
+end
+
+function Program:_setFieldOrIgnore(flag, type)
+  local option
+  if type == 'long' then
+    option = self:_findLongOption(flag)
+  elseif type == 'short' then
+    option = self:_findShortOption(flag) 
+  end
+
+  if option then
+    self:_setField(option)
+  else
+    self:_addIgnoredArg(flag)
+  end
 end
 
 function Program:parse(args)
   table.foreach(args, function (_, v)
-
     if self:_isShortFlag(v) then
       local flags = self:_expandShortFlag(v)
       for _, o in ipairs(flags) do
-        if self:_findShortOption(o) then
-          self:_setField(o)
-        else
-          self:_addIgnoredArg(o)
-        end
+        self:_setFieldOrIgnore(o, 'short')
       end
+    elseif self:_isLongFlag(v) then
+       self:_setFieldOrIgnore(v, 'long')
     else
       self:_addIgnoredArg(v)
     end
-
   end)
 end
 
--- tests/test-commander.lua
+++ tests/test-commander.lua
@@ -1,6 +1,6 @@
 --[[
 
-Copyright 2012-2014 The Luvit Authors. All Rights Reserved.
+Copyright 2015 Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -29,40 +29,72 @@ require("tap")(function (test)
     assert(program:version() == '0.0.1') 
   end)
 
+  test('Program:_splitFlags should split flags in short and log parts', function ()
+    local program = Program:new()
+    local a, all = program:_splitFlags('-a, --all')
+    assert('-a', a)
+    assert('--all', all)
+  end)
+
+  test('Program:_snakecase should return snake case of long type flags', function () 
+    local program = Program:new()
+    local allow_all = program:_snakecase('allow-all')
+    assert('allow_all' == allow_all)
+  end)
+
   test('Program:option sets new option and return the same instance', function ()
    local program = Program:new()
-   local inst = program:option('-s', 'Silent mode')
+   local inst = program:option('-s, --silent', 'Silent mode')
 
    assert(program == inst)
    assert(#program:options() == 1)
-   assert(deepEqual(program:options(), { { short = '-s', description = 'Silent mode'} }))
-   program:option('-o', 'Another option')
+   assert(deepEqual(program:options(), { { short = '-s', long = '--silent', description = 'Silent mode'} }))
+   program:option('-x, --extract', 'Another option')
    assert(#program:options() == 2)
-   assert(deepEqual(program:options(), { { short = '-s', description = 'Silent mode' }, { short = '-o', description = 'Another option' } }))
+   assert(deepEqual(program:options(), { { short = '-s', long = '--silent', description = 'Silent mode' }, { short = '-x', long = '--extract', description = 'Another option' } }))
   end)
 
-  test('Program:parse should parse short options into fields', function ()
+  --[[test('Program:option using simple bolean defaulting to false', function () 
     local program = Program:new()
-    program:option('-s', 'Silent mode')
-    program:option('-x', 'Extract')
+    program:option('
 
-    program:parse({'-s'})
-    assert(program.s)
-    assert(not program.x)
+  end)]]
+
+  test('Program:parse should set fields only for parsed and configured options', function ()
+    local program = Program:new()
+    program:option('-f, --force', 'Force update')
+    program:option('-i, --interactive', 'Interactive mode')
+    program:parse({'-i'})
+    assert(program.i)
+    assert(program.interactive)
+    assert(not program.f)
+    assert(not program.force)
   end)
 
-  test('Program:parse should leave not parsed option into args field', function () 
+  test('Program:parse should parse options into short and long fields', function ()
     local program = Program:new()
-    program:option('-t', 'Test option')
+    program:option('-s, --silent', 'Silent mode')
+    program:option('-x, --extract', 'Extract')
 
+    program:parse({'-s', '--extract'})
+    assert(program.s, 'short flag s should be set')
+    assert(program.silent, 'long flag silent sholud be set')
+    assert(program.x, 'short flag x should be set')
+    assert(program.extract, 'long flag extract should be set')
+  end)
+
+
+  test('Program:parse should leave not parsed options into args field', function () 
+    local program = Program:new()
+    program:option('-t, --test', 'Test option')
     program:parse({ '-t', '-s', 'arg2', 'arg3' })
     assert(deepEqual({'-s', 'arg2', 'arg3'}, program.args))
   end)
 
   test('Program:parse should parse collapsed short option into fields', function ()
     local program = Program:new()
-    program:option('-s', 'Silent mode')
-    program:option('-x', 'Extract')
+    program:option('-s, --silent', 'Silent mode')
+    program:option('-x, --extract', 'Extract')
     
     program:parse({'-sx', '-t'})
 
@@ -70,6 +102,5 @@ require("tap")(function (test)
     assert(program.x)
     assert(deepEqual({'-t'}, program.args))
   end)
-
 
 end)
